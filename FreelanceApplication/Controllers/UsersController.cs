e53c5a42fd4db6aa7172e39cab68456d6dc1ae3b
Enver <enver.krinickii@gmail.com>
2017-11-29 16:36:24 +0300

    button

---
+++ FreelanceApplication/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FreelanceApplication.Models;
+using Microsoft.AspNet.Identity;
+
+namespace FreelanceApplication.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationContext _context = new ApplicationContext();
+        // GET: Users
+        public ActionResult Index()
+        {
+            var id = User.Identity.GetUserId();
+            return View(_context.Users.Where(x=>x.Id == id));
+        }
+    }
+}
